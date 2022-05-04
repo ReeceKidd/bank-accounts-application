c0f54eb712343b81b8b6ef274eb5121a40ea9d56
Reece Kidd <reecekidd95@gmail.com>
2022-05-04 17:27:54 +0100

    chore: add entity diagram to read me

--- README.md
+++ README.md
@@ -1,57 +1,11 @@
-# bitcoin-wallet-api
-
-- [Objectives](#objectives)
-- [Installation](#installation)
-- [Usage](#usage)
-- [Documentation](#documentation)
-- [Testing](#testing)
-- [Improvements](#improvements)
-
-
+# Bank API and Web app
 ## Objectives
 1. Create an api to view accounts and balances
 2. Create an app to view accounts and balances
+
+## Bonus - Entity diagram
+![](./Entity-diagram.png)
  
-## Installation
-Set up the following environment variables in a .env file inside of the api folder. 
-````
-NODE_ENV=test
 
-PORT=3000
-
-ACCOUNTS_API_URL=https://bankapi.gelt.finance/accounts
-
-TRANSACTIONS_API_URL=https://bankapi.gelt.finance/transactions
-````
-Once your environment variables are set up run
-```
-yarn
-```     
-
-## Usage
-Use the below command to run the node server locally
-```
-yarn dev
-```
-
-## Documentation
-Documentation is defined inside of swagger.json
-To view the documentation run
-```
-yarn dev
-```
- and go to: 
-**http://localhost:3000/swagger**
-
-
-
-## Testing
-The API has both unit and functional tests. 
-```
- yarn test
-```
-
-## Improvements
-* Authentication middleware should be provided on service layer. 
 
 
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import './App.css';
+
 import { AccountBalance } from '../../types';
 
 const App = () => {
   const ACCOUNT_BALANCES_URL = 'http://localhost:3000/accounts/balances';
-
+  const ACCOUNT_BALANCES_NETWORK_TIMEOUT = 100000;
   const [accountBalances, setAccountBalances] = useState<AccountBalance[]>();
   const [
     fetchAccountBalancesErrorMessage,
@@ -16,7 +16,7 @@ const App = () => {
     const fetchAccountBalances = async () => {
       try {
         const accountBalancesResponse = await axios.get(ACCOUNT_BALANCES_URL, {
-          timeout: 100000
+          timeout: ACCOUNT_BALANCES_NETWORK_TIMEOUT
         });
         setAccountBalances(accountBalancesResponse.data.accountBalances);
       } catch (err) {
@@ -27,14 +27,28 @@ const App = () => {
   }, []);
   return (
     <div className="App">
-      {accountBalances?.map((accountBalance) => (
-        <p key={accountBalance.id}>
-          {accountBalance.name}-{accountBalance.totalBalance}
-        </p>
-      ))}
-      {fetchAccountBalancesErrorMessage && (
-        <p>{fetchAccountBalancesErrorMessage}</p>
-      )}
+      <div>
+        <ul>
+          {accountBalances?.map((accountBalance) => (
+            <li key={accountBalance.id}>
+              <div>
+                <img
+                  alt="profile"
+                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/488320/profile/profile-80.jpg"
+                  className="list-item-image"
+                />
+              </div>
+              <div>
+                <h4>{accountBalance.name}</h4>
+                <p>{accountBalance.totalBalance}</p>
+              </div>
+            </li>
+          ))}
+          {fetchAccountBalancesErrorMessage && (
+            <p>{fetchAccountBalancesErrorMessage}</p>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
